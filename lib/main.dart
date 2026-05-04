9ecc522f3514117cd511bfad5c79f54a3f1e1e64
Hansaja132 <isirahansajananayakkara@gmail.com>
2026-05-04 14:04:46 +0530

    Changed invalid app name in application

--- lib/main.dart
+++ lib/main.dart
@@ -78,7 +78,7 @@ class _MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Calculator',
+      title: 'Calcify',
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
         useMaterial3: true,
